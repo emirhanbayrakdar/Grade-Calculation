52175be8e92328b45b50dfa6d4a56df836112fe3
emirhanbayrakdar <106848786+emirhanbayrakdar@users.noreply.github.com>
2022-10-05 04:21:22 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,41 @@ public class Main {
         int mat, fiz, tur, kim, muz;
         Scanner scanner = new Scanner(System.in);
 
-        
-        System.out.println("Matematik Notunuz : ");
+
+        System.out.print("Matematik Notunuz : ");
         mat = scanner.nextInt();
+        if(mat<0 || mat>100){
+            System.out.print("Geçersiz not girdiniz");
+            return;
+        }
 
-        System.out.println("Fizik Notunuz : ");
+        System.out.print("Fizik Notunuz : ");
         fiz = scanner.nextInt();
+        if(fiz<0 || fiz>100){
+            System.out.print("Geçersiz not girdiniz");
+            return;
+        }
 
-        System.out.println("Türkçe Notunuz : ");
+        System.out.print("Türkçe Notunuz : ");
         tur = scanner.nextInt();
+        if(tur<0 || tur>100){
+            System.out.print("Geçersiz not girdiniz");
+            return;
+        }
 
-        System.out.println("Kimya Notunuz : ");
+        System.out.print("Kimya Notunuz : ");
         kim = scanner.nextInt();
+        if(kim<0 || kim>100){
+            System.out.print("Geçersiz not girdiniz");
+            return;
+        }
 
-        System.out.println("Müzik Notunuz : ");
+        System.out.print("Müzik Notunuz : ");
         muz = scanner.nextInt();
+        if(muz<0 || muz>100){
+            System.out.print("Geçersiz not girdiniz");
+            return;
+        }
 
         double average = (mat + fiz + kim + muz + tur) / 5;
 
